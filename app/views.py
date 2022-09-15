99b8f3f287d717f624171c9fc600e2348d3d0314
Galo Pocoma <gpocoma@msn.com>
2022-09-15 14:57:54 -0400

    Finalizado inventario de activos con código qr

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Fotografia, Activo
 # Create your views here.
+def main(request):
+    activos = Activo.objects.all()
+    #activos = Activo.objects.filter(ubicacion='Depósito 4')
+    return render(request,'index.html',{'activos':activos})
