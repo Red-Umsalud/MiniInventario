9704d727cbda86b04402dae37ca34a0a6e98bd68
Galo Pocoma <gpocoma@msn.com>
2022-09-29 14:01:49 -0400

    Agregados nuevos activos al depósito

--- app/views.py
+++ app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Fotografia, Activo
 # Create your views here.
 def main(request):
-    activos = Activo.objects.all()
-    #activos = Activo.objects.filter(ubicacion='Depósito 4')
+    #activos = Activo.objects.all()
+    activos = Activo.objects.filter(ubicacion='Depósito 6')
     return render(request,'index.html',{'activos':activos})
