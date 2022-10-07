85a73db1f85022fb696272ed033ab30f06feac99
Galo Pocoma <gpocoma@msn.com>
2022-10-07 09:55:02 -0400

    Agregada informacion de escaner

--- app/views.py
+++ app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Fotografia, Activo
 # Create your views here.
 def main(request):
-    activos = Activo.objects.all()
-    #activos = Activo.objects.filter(ubicacion='Depósito 6')
+    #activos = Activo.objects.all()
+    activos = Activo.objects.filter(ubicacion='Depósito 7')
     return render(request,'index.html',{'activos':activos})
